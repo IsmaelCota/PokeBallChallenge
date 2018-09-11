97ed3942fd7dfca05c8efc8db387459ea77d3c8f
IsmaelCota <36893961+IsmaelCota@users.noreply.github.com>
2018-09-11 08:26:09 -0700

    Source Code
    Code for PokeBall

---
+++ PokeBall.java
@@ -0,0 +1,17 @@
+
+public class PokeBall {
+
+	public static void main(String[] args) {
+		 System.out.println("   @@@@@@@@@@");
+		 System.out.println("  @@@@@@@@@@@@ ");
+		 System.out.println(" @@@@@@@@@@@@@@");
+		 System.out.println("@@@@@@@@@@@@@@@@");
+		 System.out.println("|||||[““““]|||||");
+		 System.out.println("|||||[....]|||||");
+		 System.out.println("OOOOOOOOOOOOOOOO");
+		 System.out.println(" OOOOOOOOOOOOOO");
+		 System.out.println("  OOOOOOOOOOOO");
+		 System.out.println("   OOOOOOOOOO");
+	}
+
+}
